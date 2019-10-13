cf680d69f1117e84651ea4af2e9544de758a46a4
Dishant Kaushik <dkaushik95@gmail.com>
2019-10-13 19:14:48 -0400

    added a module structure

--- Coding Assignment 1/backend/index.js
+++ Coding Assignment 1/backend/index.js
@@ -1,47 +1,29 @@
 const cluster = require('cluster')
 const http = require('http')
 const numCPUs = require('os').cpus().length
+const navigation = require('./modules/navigation')
+const communication = require('./modules/communication')
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`)
-
+  const modules = ['navigation', 'communications', 'controls']
   // fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
-
-  for (const id in cluster.workers) {
-    cluster.workers[id].on('message', msg => {
-      console.log(`${msg.pid} :: isAlive: ${msg.isAlive}`)
+  modules.forEach(mod => {
+    cluster.fork({
+      moduleType: mod
     })
-  }
+  })
 
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`)
   })
-
-  setInterval(() => {
-    for (const id in cluster.workers) {
-      cluster.workers[id].send({
-        type: 'isAlive'
-      })
-    }
-  }, 5000)
 } else {
-  http
-    .createServer((req, res) => {
-      console.log(req.url, process.pid)
-      res.writeHead(200)
-      res.end('Hello world\n')
-    })
-    .listen(8000)
-  process.on('message', msg => {
-    if (msg.type === 'isAlive') {
-      process.nd({
-        isAlive: true,
-        pid: process.pid
-      })
-    }
-  })
-  console.log(`Worker ${process.pid} started`)
+  switch (process.env.moduleType) {
+    case 'navigation':
+      navigation.init()
+      break
+    case 'communications':
+      communication.init()
+      break
+  }
 }
