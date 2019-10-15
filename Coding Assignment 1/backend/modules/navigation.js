98f9334d3df4656f57301bc45da07f212bab635e
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2019-10-15 13:28:00 -0400

    Change in lofic for heartbeat, working fine

--- Coding Assignment 1/backend/modules/navigation.js
+++ Coding Assignment 1/backend/modules/navigation.js
@@ -6,12 +6,14 @@ var navObj = {
 function generateCoordinates() {
   let minLongitude = -90.0
   let maxLongitude = 90.0
-  let minLatitude = -180.0
+  let minLatitude = 0.0
   let maxLatitude = 180.0
 
   let latitude = minLatitude + Math.random() * (maxLatitude - minLongitude + 1)
   let longitude =
     minLongitude + Math.random() * (maxLongitude - minLongitude + 1)
+
+  console.log('latitude', latitude, 'longitude', longitude)
 
   if (latitude > 89.8 && longitude < 0.2) {
     console.log('Critical process died')
