ee98782ef74155ba138a88ffc957ad346cd9a083
Raseshwari <rp3737@rit.edu>
2019-10-14 23:19:27 -0400

    receiver code

---
+++ Coding Assignment 1/backend/heartbeatreciever.js
@@ -0,0 +1,44 @@
+const cluster = require('cluster')
+const http = require('http')
+const numCPUs = require('os').cpus().length
+const navigation = require('./modules/navigation')
+const communication = require('./modules/communication')
+const imageprocessing = require('./modules/imageprocessing')
+const modules = ['navigation', 'communications', 'imageprocessing']
+
+var lastUpdatedTime;
+const expirationTime = 6;
+
+function updateTime(){
+  var currentTime = new Date();
+  lastUpdatedTime = navigation.navObj.time;
+  console.log(currentTime.getSeconds() - lastUpdatedTime.getSeconds()) 
+  setTimeout(updateTime, 4000);
+}
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
+  // fork workers
+  modules.forEach(mod => {
+    cluster.fork({
+      moduleType: mod
+    })
+  })
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  switch (process.env.moduleType) {
+    case 'navigation':
+      navigation.init()
+      updateTime();
+      break
+    case 'communications':
+      communication.init();
+      break
+    case 'imageprocessing':
+      imageprocessing.init()
+      break
+  }
+}
---
+++ Coding Assignment 1/backend/modules/imageprocessing.js
@@ -0,0 +1,5 @@
+module.exports = {
+    init: () =>{
+        console.log(`I am ${process.env.moduleType} from image processing ${process.pid}`)
+    }
+}
